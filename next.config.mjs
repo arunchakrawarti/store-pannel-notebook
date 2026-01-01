147a5a94b4d7e6e9f6666986d0c8ad8fd9d61c59
Arun Chakrawarti <arunchakrawarti@gmail.com>
2026-01-01 15:28:53 +0530

    push

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
+
+import { join } from "path";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  webpack: (config) => {
+    config.resolve.alias["@"] = join(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
