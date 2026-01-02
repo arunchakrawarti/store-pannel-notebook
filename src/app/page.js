68d9eb4a04d26903f88825be2834689da0df5caa
Arun Chakrawarti <arunchakrawarti@gmail.com>
2026-01-02 10:38:26 +0530

    feat:fix vercel issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Homepage from "@/components/pages/home";
 
 export default function Home() {
   return (
     <div>
-      <Homepage />
+      jsbxjbs
+      {/* <Homepage /> */}
     </div>
   );
 }
